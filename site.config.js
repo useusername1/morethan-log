35101cff2861bae7e44ae1b3c79e5e18edbda8cb
useusername1 <reknvqvjoq123@gmail.com>
2023-08-10 22:32:41 +0900

    chore: change config

--- site.config.js
+++ site.config.js
@@ -1,31 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    name: "김진주",
+    image: "/powerpuff.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "I'm a frontend engineer.",
+    email: "jjkim12321@gmail.com",
+    github: "useusername1",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Pikcha 1.1`,
+      href: "https://github.com/codestates-seb/seb41_main_036",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "jjk_code_code",
     description: "welcome to morethan-log!",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
